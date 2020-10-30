6ee1558adb26b9702709ca5f1246d29b1b13087e
Chu <chuh5@nih.gov>
2020-10-30 16:29:23 -0400

    added graphics for minimal POC

--- R/compile.dac.report.R
+++ R/compile.dac.report.R
@@ -5,21 +5,36 @@ compile.dac.report <- function(dac, author, start.date, end.date,...) {
   title <- paste(toupper(dac), "Data Access Committee dbGaP Activity Report \n", paste(start.date,end.date,sep="-"), sep=" ")
 
   timeline.summary.table <- dar.review.timeline.summary(start.date,end.date)
+  print(timeline.summary.table)
   dac.row <- timeline.summary.table[timeline.summary.table['DAC'] == dac,]
-  print(dac.row)
+
+  num.dars.in.window <- dac.row[,'TotalRequests']
+  number.of.accepted <- dac.row[,'TotalApproved']
+  number.of.rejected <- dac.row[,'TotalRejected']
+
+  avg.accept <- dac.row[,'AvgApprovalTime']
+  avg.reject <- dac.row[,'AvgRejectionTime']
+
+  dac.total.dars <- nrow(nih_dac_action_table[nih_dac_action_table['DAC'] == dac,])
+  dac.total.project <- length(unique(nih_dac_action_table[nih_dac_action_table['DAC'] == dac,]['Project']))
+
+  avg.final <- (avg.accept*number.of.accepted + avg.reject*number.of.rejected) / (number.of.accepted+number.of.rejected)
+
+
   rmarkdown::render(this.file, params = list(
   title = title,
   start.date = start.date,
   end.date = end.date,
   author = author,
   dac=dac,
-  total.number.of.dars=0,
-  total.number.of.projects=0,
-  num.dars.in.window=dac.row['TotalRequests'],
-  number.of.accepted=dac.row['TotalApproved'],
-  number.of.rejected=dac.row['TotalRejected'],
-  avg.final=0,
-  avg.accept=dac.row['AvgApprovalTime'],
-  avg.reject=dac.row['AvgRejectionTime']
+  total.number.of.dars=dac.total.dars,
+  total.number.of.projects=dac.total.project,
+  num.dars.in.window=num.dars.in.window,
+  number.of.accepted=number.of.accepted,
+  number.of.rejected=number.of.rejected,
+  avg.final=round(avg.final,2),
+  avg.accept=round(avg.accept,2),
+  avg.reject=round(avg.reject,2),
+  timeline.summary.table=timeline.summary.table
    ),...)
 }
