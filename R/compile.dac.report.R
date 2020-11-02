1c352203ea18877a8f712fa18d7a583ac5404d80
Chu <chuh5@nih.gov>
2020-11-02 09:11:10 -0500

    added documentation for compile.dac.report

--- R/compile.dac.report.R
+++ R/compile.dac.report.R
@@ -1,3 +1,28 @@
+#' Compile DAC Report
+#'
+#' Creates a report on the DAR (Data Access Request) status of the given DAC
+#' (Data Access Commitee) and compare it to the rest of the NIH institutes. The
+#' generated report will be a word document.
+#'
+#' @param dac String, the name of the DAC from which the report will be generated for, one of
+#' "CDAC", "ES DAC", "JAAMH", "Kids First DAC", "NCATS", "NCI DAC", "NEI", "NHGRI",
+#' "NHLBI", "NIAID", "NIAMS", "NICHD", "NIDCD DAC", "NIDCR", "NIDDK", "NIGMS", "NINDS"
+#' "NINR DAC"
+#'
+#' @param author String, The name of the author who created this report
+#' @param start.date String, date in "yyyy-mm-dd" format, the start date of the timeframe
+#' in which the analysis will be based on
+#' @param end.date String, date in "yyyy-mm-dd" format, the end date of the timeframe in
+#' which the analysis will be based on
+#' @param ... extra arguments to be provided to rmarkdown::render
+#'
+#' @return NULL
+#'
+#' @examples \dontrun{
+#' compile.dac.report('NIAID','Hoyin Chu, Christopher Marcum', '2015-01-01', '2019-12-31')
+#' }
+#'
+#' @export
 compile.dac.report <- function(dac, author, start.date, end.date,...) {
 
   this.file<- system.file("inst", "report.Rmd", package = "DACReportingTool")
