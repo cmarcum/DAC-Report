ed42ca82d98c0a7d0e5423899d59e019c9e44509
Chu <chuh5@nih.gov>
2020-11-12 15:07:25 -0500

    added new figure and reorganized report

--- R/GetApprovalTimeMovingAverage.R
+++ R/GetApprovalTimeMovingAverage.R
@@ -3,8 +3,10 @@
 get.approval.time.moving.average <- function(start.date,end.date,action.table.df=nih_dac_action_table) {
   temp.action.table.df <- get.df.within.range(action.table.df,start.date,end.date,date.col="Approved by DAC")
   temp.action.table.df$ApprovedMonth <- lubridate::floor_date(as.Date(temp.action.table.df[,'Approved by DAC'], format = "%m/%d/%Y"),"month")
-  temp.action.table.df$ApprovalTime <- difftime(to.time(temp.action.table.df[,"Approved by DAC"]), to.time(temp.action.table.df[,"Submitted by PI"]),units = "days")
-  temp.action.table.df$ApprovalTimeFromSO <- difftime(to.time(temp.action.table.df[,"Approved by DAC"]), to.time(temp.action.table.df[,"Approved by SO"]),units = "days")
+  temp.action.table.df$ApprovalTimeFromPIToDAC <- difftime(to.time(temp.action.table.df[,"Approved by DAC"]), to.time(temp.action.table.df[,"Submitted by PI"]),units = "days")
+  temp.action.table.df$ApprovalTimeFromPIToSO <- difftime(to.time(temp.action.table.df[,"Approved by SO"]), to.time(temp.action.table.df[,"Submitted by PI"]),units = "days")
+  temp.action.table.df$ApprovalTimeFromSOToDAC <- difftime(to.time(temp.action.table.df[,"Approved by DAC"]), to.time(temp.action.table.df[,"Approved by SO"]),units = "days")
+
   return(temp.action.table.df)
   #
   #   monthly.study.approved.df <- stats::aggregate(temp.action.table.df['ApprovalTime'], by=list(Month=temp.action.table.df$ApprovedMonth), FUN=mean)
