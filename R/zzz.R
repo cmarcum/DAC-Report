c2b7a478baed0221501cdfc02332b373cf24f0a0
Chu <chuh5@nih.gov>
2020-11-19 09:17:02 -0500

    update package onload message

--- R/zzz.R
+++ R/zzz.R
@@ -1,11 +1,11 @@
 .onLoad <- function(libname,pkgname) {
-  msg <- "This package is intended for use by NIH Data Access Committees.
-Written and maintained by Hoyin Chu and Christopher Steven Marcum
-chris.marcum@nih.gov. Support for the development of this package was
-provided by Civic Data Fellows Program of the Office of Science Policy
-at the National Institutes of Health and the Office of Data Science and
-Emerging Technology at the National Institute of Allergies and Infectious
-Diseases."
+  msg <- "This package is intended for use by NIH Data Access Committees. Written
+and maintained by Hoyin Chu and Christopher Steven Marcum chris.marcum@nih.gov.
+This project was funded by the National Institutes of Health through the Office
+of Data Science Strategy in partnership with the Coding It Forward Civic Digital
+Fellows Program and the Office of Data Science and Emerging Technologies at the
+  National Institute of Allergy and Infectious Diseases"
+
   update.msg <- "Run get.latest.approved.dar.date() to get the latest date in
 locally stored data.\nRun update.all.tables() to update to a more recent version
 if necessary."
