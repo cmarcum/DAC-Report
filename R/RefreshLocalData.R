0bf6d997682172038568fdf82738df4503693013
Chu <chuh5@nih.gov>
2020-12-02 17:50:13 -0500

    added function to refresh data

--- R/RefreshLocalData.R
+++ R/RefreshLocalData.R
@@ -13,19 +13,22 @@
 #' refresh.local.data()
 #' }
 #'
-refresh.local.data <- function(start.date=as.Date("2000-01-01"),end.date=Sys.Date()) {
+refresh.local.data <- function(start.date=as.Date("2000-01-01"),end.date=Sys.Date(),gap="1 year") {
   print("Downloading tables to be used locally for DACReportingTool...")
-  date.ranges <- as.list(seq(start.date,end.date,by="1 year"))
-  date.ranges[[length(date.ranges) + 1]] <- end.date
-  table.list <- list()
-  for (i in 1:(length(date.ranges)-1)) {
-    action.table <- request.all.dac.action.table(format(date.ranges[[i]],"%m/%d/%Y"),format(date.ranges[[i+1]],"%m/%d/%Y"))
-    if (nrow(action.table) > 0) {
-      table.list[[i]] <- action.table
-    }
-  }
-  nih_dac_action_table <- data.table::rbindlist(table.list,fill = TRUE)
+  # date.ranges <- as.list(seq(start.date,end.date,by=gap))
+  # date.ranges[[length(date.ranges) + 1]] <- end.date
+  # table.list <- list()
+  # for (i in 1:(length(date.ranges)-1)) {
+  #   action.table <- request.all.dac.action.table(format(date.ranges[[i]],"%m/%d/%Y"),format(date.ranges[[i+1]],"%m/%d/%Y"))
+  #   if (nrow(action.table) > 0) {
+  #     table.list[[i]] <- action.table
+  #   }
+  # }
+  # nih_dac_action_table <- data.table::rbindlist(table.list,fill = TRUE)
+  nih_dac_action_table <- request.all.dac.action.table(format(start.date,"%m/%d/%Y"),format(end.date,"%m/%d/%Y"))
   nih_dac_action_table <- nih_dac_action_table[!duplicated(nih_dac_action_table$DAR, fromLast=T),]
+
+  # return(nih_dac_action_table)
 
   all_nih_dac_studies_table <- request.all.nih.dac.studies.table(format(start.date,"%m/%d/%Y"),format(end.date,"%m/%d/%Y"))
 
